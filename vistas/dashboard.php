32d6694ef0bea605c8f0abb36ea6a0b9c0daba35
Eva Blanco <51144190+Evablan@users.noreply.github.com>
2025-03-21 15:59:31 +0100

    Estoy empezando a crear el dashboard.php que es la zona principal que ve el usuario después de iniciar sesión

--- vistas/dashboard.php
+++ vistas/dashboard.php
@@ -1,1 +1,17 @@
 <!-- Panel de usuario -->
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Dashboard</title>
+</head>
+
+<body>
+
+    <h1>Panel de Usuario</h1>
+
+</body>
+
+</html>
