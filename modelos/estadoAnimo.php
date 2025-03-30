8f697f847cd86a598897a046c810cf6062515a31
Eva Blanco <51144190+Evablan@users.noreply.github.com>
2025-03-30 21:37:16 +0200

    He empezado el archivo estadoAnimo.php en el modelo

--- modelos/estadoAnimo.php
+++ modelos/estadoAnimo.php
@@ -1,1 +1,26 @@
 <!-- Interacción con la base de datos -->
+
+<!--1. Conexión a la bbdd -->
+<?php
+require_once __DIR__ . '/../config/dataBase.php';
+
+/*2. Definir una clase estadoAnimo() que será la responsable de la lógica de guardado*/
+
+class estadoAnimo
+{
+    public static function guardar($usuario_id, $estado_animo, $texto_diario)
+    {
+        global $pdo; //Usamos la conexión $pdo desde dataBase.php  
+        $sql = "INSERT INTO tabla estados_animo(usuario_id, estado_animo, texto_diario,fecha) VALUES (?,?,?NOW())";
+        $stm = $pdo->prepare($sql);
+        $stm->execute([$usuario_id, $estado_animo, $texto_diario]);
+    }
+}
+
+
+
+
+
+
+
+?>
